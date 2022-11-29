f2dcca894234f9b225797890b01755a783055c2a
MarcusGuanOkta <86582210+MarcusGuanOkta@users.noreply.github.com>
2022-11-29 18:17:33 +0800

    Create profile.js
    profile.js with update button and update function

---
+++ profile.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+import Highlight from "../components/Highlight";
+import Loading from "../components/Loading";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
+
+export const ProfileComponent = () => {
+    const { user, getAccessTokenSilently } = useAuth0();
+
+    const update = async () => {
+        //get access token
+        const accesstoken = await getAccessTokenSilently();
+        //get color from html input field
+        const color = await document.getElementById("color").value;
+        //update color to user's metadata
+        const updateEndPoint = `https://cic_domain/api/v2/users/${user.sub}`;
+        const metadataResponse = await fetch(updateEndPoint, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${accesstoken}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'user_metadata': {
+                    'color': `${color}`
+                }
+            })
+        });
+    }
+
+
+  return (
+    <Container className="mb-5">
+      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
+        <Col md={2}>
+          <img
+            src={user.picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </Col>
+        <Col md>
+          <h2>{user.name}</h2>
+                  <p className="lead text-muted">{user.email}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
+      </Row>
+          <div>
+              <form>
+                  <label>favorite color:  </label>
+                  <input type="text" id="color"></input>
+                  <input type="submit" value="update" onClick={update}></input>
+              </form>
+          </div>
+
+      </Container>
+
+
+  );
+};
+
+
+
+export default withAuthenticationRequired(ProfileComponent, {
+  onRedirecting: () => <Loading />,
+});
